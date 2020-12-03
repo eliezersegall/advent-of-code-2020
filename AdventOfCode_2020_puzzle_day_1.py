9db3a612f9c3643d31ac8dc3f3c0ecbe57f26bca
Eliezer segall <75254033+eliezersegall@users.noreply.github.com>
2020-12-03 12:42:25 +0200

    Add files via upload

---
+++ AdventOfCode_2020_puzzle_day_1.py
@@ -0,0 +1,31 @@
+def main():
+    with open('D:\ztmDevelop\day1input.txt','r') as string:
+        str = string.read()
+    list = ['0']
+    n = 0
+    for i in str:
+        if i != '\n':
+            list[n] += i
+        else:
+            list += '0'
+            n += 1
+
+    found = False
+    for i in range(len(list)):
+        for k in range(len(list)):
+            if not(found) and (i != k) and (int(list[i]) + int(list[k]) == 2020):
+                print('The pair is: %d + %d = 2020' % (int(list[i]), int(list[k])))
+                print("The pair's product: %d * %d = %d" % (int(list[i]), int(list[k]), int(list[i])*int(list[k])))
+                found = True
+                
+    found = False
+    for i in range(len(list)):
+        for k in range(len(list)):
+            for g in range(len(list)):
+                    if not(found) and (i != k) and (i != g) and (g != k) and (int(list[i]) + int(list[k]) + int(list[g]) == 2020):
+                        print('The trio is: %d + %d + %d = 2020' % (int(list[i]), int(list[k]), int(list[g])))
+                        print("The trio's product: %d * %d * %d = %d" % (int(list[i]), int(list[k]), int(list[g]), int(list[i])*int(list[k])*int(list[g])))
+                        found = True
+    input()
+if __name__ == "__main__":
+    main()
