f93636801ea931b09f7f06de27e6cdc18393273c
Eliezer segall <75254033+eliezersegall@users.noreply.github.com>
2020-12-06 08:52:18 +0200

    Add files via upload

---
+++ AdventOfCode_2020_puzzle_day_6.py
@@ -0,0 +1,26 @@
+def main():
+    with open('D:\ztmDevelop\AdventOfCode_2020\day6input.txt','r') as string:
+        str = string.read()
+    list = str.split('\n\n')
+    part_1_sum = 0
+    for i in range(len(list)):
+        list[i] = list[i].replace('\n','')
+        for k in range(97, 123):
+            list[i] = list[i].replace(chr(k),'',list[i].count(chr(k))-1)
+        part_1_sum += len(list[i])
+    print("The sum of all groups' answers: %d" % part_1_sum)
+    part_2_sum = 0
+    list = str.split('\n\n')
+    l_list = str.split('\n\n')
+    for i in range(len(list)):
+        list[i] = list[i].replace('\n','')
+        l_list[i] = l_list[i].split('\n')
+        for k in range(97, 123):
+            if list[i].count(chr(k)) == len(l_list[i]):
+                part_2_sum += 1
+    
+    print("The sum of all groups' SHARED answers: %d" % part_2_sum)
+
+    input()
+if __name__ == "__main__":
+    main()
